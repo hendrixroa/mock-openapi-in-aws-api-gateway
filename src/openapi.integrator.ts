20ece261aae5fa437cfdce3f1d17e162e7a4391b
Hendrix Roa <hendrix.alex87@gmail.com>
2021-03-08 13:37:16 -0500

    updates

--- src/openapi.integrator.ts
+++ src/openapi.integrator.ts
@@ -1,4 +1,9 @@
-import SwaggerParser from "@apidevtools/swagger-parser";
+import * as SwaggerParser from "@apidevtools/swagger-parser";
+import * as jsf from 'json-schema-faker';
+import * as faker from 'faker';
+
+jsf.format('binary', () => null);
+jsf.format('uuid', () => faker.random.uuid());
 
 export class OpenapiIntegrator {
   private readonly apiSpec: any;
@@ -10,6 +15,10 @@ export class OpenapiIntegrator {
   public async addIntegration() {
     await this.validateOpenapiSpec();
     let apiSpecMutable: any = this.apiSpec;
+    const components = apiSpecMutable.components;
+    const data = await jsf.resolve(components.schemas);
+
+    console.log('data: ', data);
 
     Object.keys(apiSpecMutable.paths).map((path: string) => {
       Object.keys(apiSpecMutable.paths[path]).map((method: string) => {
@@ -23,7 +32,7 @@ export class OpenapiIntegrator {
       await SwaggerParser.validate(this.apiSpec);
     }
     catch(err) {
-      throw new Error('Invalid api spec');
+      throw new Error('Invalid api spec: ' + err);
     }
   }
 }
